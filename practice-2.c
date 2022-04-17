45dff247bbee971a8089dec7a329e3d8d6074af5
akari <terrorblade047@gmail.com>
2022-04-17 01:22:42 -0700

    Solved problem #9

--- practice-2.c
+++ practice-2.c
@@ -11,12 +11,12 @@ int main()
     // ELSE IF num3 > num1 && num2, then print out num3 as output
     // ELSE print out n and n are the same values
 
-    int num1, num2, num3;
+    // int num1, num2, num3;
 
-    printf("\nInput the three numbers to compare: ");
-    scanf("%d %d %d", &num1, &num2, &num3);
+    // printf("\nInput the three numbers to compare: ");
+    // scanf("%d %d %d", &num1, &num2, &num3);
 
-    printf("1st number: %d, 2nd number: %d, 3rd number: %d", num1, num2, num3);
+    // printf("1st number: %d, 2nd number: %d, 3rd number: %d", num1, num2, num3);
 
     // if(num1 > num2 && num3){
     //     printf("\n\nThe 1st number: %d, is the greatest among the three.", num1);
@@ -31,23 +31,55 @@ int main()
     //     printf("\n\nThe value of %d, %d, %d are the same.", num1, num2, num3);
     // }
 
-    if(num1 > num2){
-        if(num1 > num3){
-            printf("\nThe 1st number: %d, is the greatest among the three.", num1);
-        }
-        else{
-            printf("\nThe 3rd number: %d, is the greatest among the three.", num3);
-        }
-    }
-    else if(num2 > num3){
-        printf("\nThe 2nd number: %d, is the greatest among the three.", num2);
-    }
-    else if(num2 < num3){
-        printf("\nThe 3rd number:%d, is the greatest among the three.", num3);
-    }
-    else{
-        printf("\nThe three values you put in are the same");
-    }
+    // if(num1 > num2){
+    //     if(num1 > num3){
+    //         printf("\nThe 1st number: %d, is the greatest among the three.", num1);
+    //     }
+    //     else{
+    //         printf("\nThe 3rd number: %d, is the greatest among the three.", num3);
+    //     }
+    // }
+    // else if(num2 > num3){
+    //     printf("\nThe 2nd number: %d, is the greatest among the three.", num2);
+    // }
+    // else if(num2 < num3){
+    //     printf("\nThe 3rd number:%d, is the greatest among the three.", num3);
+    // }
+    // else{
+    //     printf("\nThe three values you put in are the same");
+    // }
  
-    return 0;    
+    /* 9. Write a C program to accept a coordinate point in a XY coordinate system 
+    and determine in which quadrant the coordinate point lies */
+
+    // Prompt user for 2 input
+    // Quadrant 1 == ++, quadrant 2 == -+, quadrant 3 == --, quadrant 4 == +-
+    // IF n1 && n2 > 0, print out output as quadrant 1
+    // ELSE IF n1 < 0, and n2 >= 0, print output as quadrant 2
+    // ELSE IF n1 && n2 < 0, print output as quadrant 3
+    // ELSE print output as quadrant 4
+
+    // int x, y;
+    // printf("Input the two values: ");
+    // scanf("%d %d", &x, &y);
+
+    // if((x > 0 && y > 0)){
+    //     printf("The coordinate point of (%d,%d) lies in the First quadrant", x, y);
+    // }
+    // else if((x < 0 && y > 0)){
+    //     printf("The coordinate point of (%d,%d) lies in the Second quadrant", x, y);
+    // }
+    // else if((x < 0 && y < 0)){
+    //     printf("The coordinate point of (%d,%d) lies in the Third quadrant", x, y);
+    // }
+    // else if((x == 0 && y == 0)){
+    //     printf("The coordinate point of (%d,%d) lies at the Origin", x, y);
+    // }
+    // else{
+    //     printf("The coordinate point of (%d,%d) lies in the Fourth quadrant", x, y);
+    // }
+    
+
+
+    return 0; 
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+ // if(num1 > num2){
+    //     if(num1 > num3){
+    //         printf("\nThe 1st number: %d, is the greatest among the three.", num1);
+    //     }
+    //     else{
+    //         printf("\nThe 3rd number: %d, is the greatest among the three.", num3);
+    //     }
+    // }
+    // else if(num2 > num3){
+    //     printf("\nThe 2nd number: %d, is the greatest among the three.", num2);
+    // }
+    // else if(num2 < num3){
+    //     printf("\nThe 3rd number:%d, is the greatest among the three.", num3);
+    // }
+    // else{
+    //     printf("\nThe three values you put in are the same");
+    // }
+ 
