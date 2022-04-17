cb810180774bc4c15ae6ea5e1ce225fdcad11be9
akari <terrorblade047@gmail.com>
2022-04-17 00:58:41 -0700

    Solved problem #8

--- practice-2.c
+++ practice-2.c
@@ -32,20 +32,22 @@ int main()
     // }
 
     if(num1 > num2){
-        if(num1 > num3)
+        if(num1 > num3){
             printf("\nThe 1st number: %d, is the greatest among the three.", num1);
-        
-        else
+        }
+        else{
             printf("\nThe 3rd number: %d, is the greatest among the three.", num3);
-
-    else if (num2 > num3)
+        }
+    }
+    else if(num2 > num3){
         printf("\nThe 2nd number: %d, is the greatest among the three.", num2);
-
-    else if(num2 < num3)
+    }
+    else if(num2 < num3){
         printf("\nThe 3rd number:%d, is the greatest among the three.", num3);
-    
-    else 
+    }
+    else{
         printf("\nThe three values you put in are the same");
- }
+    }
+ 
     return 0;    
 }
